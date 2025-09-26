fdacf84e283d4bebfde52609fd2d6b376b40fb4b
podo3501 <nixie77@naver.com>
2025-09-26 18:19:57 +0900

    sdl3 라이브러리 테스트 중. ime 및 사운드를 사용하기 위해서 필요함.

---
+++ ToyTest/CodeLab/SDL3.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "SDL3/SDL.h"
+
+namespace b_CodeLab
+{
+	TEST(SDL3, Initialize)
+	{
+        //EXPECT_TRUE(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO));
+
+        ////Set audio spec
+        //SDL_AudioSpec audioSpec;
+        //SDL_zero(audioSpec);
+        //audioSpec.format = SDL_AUDIO_F32;
+        //audioSpec.channels = 2;
+        //audioSpec.freq = 44100;
+
+        ////Open audio device
+        //SDL_AudioDeviceID gAudioDeviceId = SDL_OpenAudioDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &audioSpec);
+        //EXPECT_TRUE(gAudioDeviceId != 0);
+
+	}
+}
