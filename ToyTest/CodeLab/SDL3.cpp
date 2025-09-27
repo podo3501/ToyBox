3eab1648ddc623dd8b6bcb637cea6f9fdaa8d170
podo3501 <nixie77@naver.com>
2025-09-27 18:45:39 +0900

    sdl static으로 빌드후 Release, Debug별로 나누어서 연결하게끔 수정. 추후에 그냥 테스트만 하는 프로젝트를 따로 붙여서 sdl테스트 하는 코드는 그쪽으로 옮길 예정

--- ToyTest/CodeLab/SDL3.cpp
+++ ToyTest/CodeLab/SDL3.cpp
@@ -1,22 +1,24 @@
 #include "pch.h"
 #include "SDL3/SDL.h"
 
+//그냥 테스트 할려고 만들기는 했지만, 이렇게 테스트 하는건 올바른 방법이 아니다.
+//나중에 이 테스트는 지울 예정. 
 namespace b_CodeLab
 {
 	TEST(SDL3, Initialize)
 	{
-        //EXPECT_TRUE(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO));
+        EXPECT_TRUE(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO));
 
-        ////Set audio spec
-        //SDL_AudioSpec audioSpec;
-        //SDL_zero(audioSpec);
-        //audioSpec.format = SDL_AUDIO_F32;
-        //audioSpec.channels = 2;
-        //audioSpec.freq = 44100;
+        //Set audio spec
+        SDL_AudioSpec audioSpec;
+        SDL_zero(audioSpec);
+        audioSpec.format = SDL_AUDIO_F32;
+        audioSpec.channels = 2;
+        audioSpec.freq = 44100;
 
-        ////Open audio device
-        //SDL_AudioDeviceID gAudioDeviceId = SDL_OpenAudioDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &audioSpec);
-        //EXPECT_TRUE(gAudioDeviceId != 0);
+        //Open audio device
+        SDL_AudioDeviceID gAudioDeviceId = SDL_OpenAudioDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, &audioSpec);
+        EXPECT_TRUE(gAudioDeviceId != 0);
 
 	}
 }
